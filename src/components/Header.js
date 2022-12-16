c4364a26f7c85939ef7e6cd3b31260c7881482c2
Victoria Lim <lim.t.victoria@gmail.com>
2022-12-16 11:23:53 -0800

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import logo from '../assets/images/logo.svg';
+
+const Header = (props) => (
+    <header id="header" className="alt">
+        <span className="logo"><img src={logo} alt="" /></span>
+        <h1>LetterLift</h1>
+        <p>Professional Editing Services</p>
+    </header>
+)
+
+export default Header
