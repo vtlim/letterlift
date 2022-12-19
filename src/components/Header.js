8475e18483d08dd1b3d7e2a152f3e729c13777e3
Victoria Lim <lim.t.victoria@gmail.com>
2022-12-18 22:28:58 -0800

    add logo (temp png) of accent5 color; update accent5

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/logo.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
