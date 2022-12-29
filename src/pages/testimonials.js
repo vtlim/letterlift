c9156beec273f0e094ec34c35f7d2aceb0e51378
Victoria Lim <lim.t.victoria@gmail.com>
2022-12-29 14:02:20 -0800

    testimonials layout css and js

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
 import HeaderTestimonials from '../components/HeaderTestimonials'
+import overlay from '../assets/images/overlay.png'
 import pic04 from '../assets/images/pic04.jpg'
 
 class Testimonials extends React.Component {
@@ -15,10 +16,68 @@ class Testimonials extends React.Component {
         <div id="main">
           <section id="content" className="main">
             <span className="image main"><img src={pic04} alt="" /></span>
-            <h2>Feedback from those who worked with LetterLift</h2>
-            <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-            <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
-            <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+            <div class="left_testimonial">
+              <div class="praise">
+                <p>When I was writing my personal statement to apply to graduate schools for an MS in bioinformatics, I was <b>overwhelmed by the lack of structure</b> provided by the institutions I was applying to. I had a sense of the story I wanted to tell but was <b>struggling to synthesize the ideas in a way that was aligned with what the admissions officers were seeking</b>.</p>
+                <p>Getting feedback on something deeply personal is uncomfortable and I don’t take criticism well. When we were working on my personal statement, I really got the sense that we were <b>working as a team</b>. She was always incredibly careful to make sure she understood what I was trying to communicate before she gave feedback which made the process strangely pleasant. She also has this otherworldly ability to <b>distill what the other side of the table is looking for</b> and restructure the document accordingly, which helped me enormously to alter the tone and craft bespoke personal statements for each institution.</p>
+                <p>Working with Victoria was a game changer. In a little over an hour we were able to revise my personal statement in a way that I was able to tell my story but in a vastly more concise, targeted, and personalized manner.<b> In the end I was accepted into Master’s programs for Johns Hopkins, Northeastern, and NYU.</b></p>
+              </div>
+              <div class="headshot">
+                <span className="image">
+                  <img src={overlay} alt="" />
+                </span>
+              </div>
+              <div class="title">
+                <div><a href="https://www.linkedin.com/in/alex-willenbrink-707856104/"><b>Alex W.</b></a>
+                <br />Software Engineer II at Illumina<br /><i>Applied to masters programs in bioinformatics</i></div>
+              </div>
+            </div>
+            <hr />
+            <div class="right_testimonial">
+              <div class="praise">
+                <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
+                <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
+              </div>
+              <div class="headshot">
+                <span className="image">
+                  <img src={overlay} alt="" />
+                </span>
+              </div>
+              <div class="title">
+                <div><a href="https://www.linkedin.com/in/alex-willenbrink-707856104/"><b>Alex W.</b></a>
+                <br />Software Engineer II at Illumina<br /><i>Applied to masters programs in bioinformatics</i></div>
+              </div>
+            </div>
+            <hr />
+            <div class="left_testimonial">
+              <div class="praise">
+                <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
+              </div>
+              <div class="headshot">
+                <span className="image">
+                  <img src={overlay} alt="" />
+                </span>
+              </div>
+              <div class="title">
+                <div><a href="https://www.linkedin.com/in/alex-willenbrink-707856104/"><b>Justin S.</b></a>
+                <br />Software Engineer II at Illumina<br /><i>Applied to masters programs in bioinformatics</i></div>
+              </div>
+            </div>
+            <hr />
+            <div class="right_testimonial">
+              <div class="praise">
+                <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
+              </div>
+              <div class="headshot">
+                <span className="image">
+                  <img src={overlay} alt="" />
+                </span>
+              </div>
+              <div class="title">
+                <div><a href="https://www.linkedin.com/in/alex-willenbrink-707856104/"><b>Alex W.</b></a>
+                <br />Software Engineer II at Illumina<br /><i>Applied to masters programs in bioinformatics</i></div>
+              </div>
+            </div>
           </section>
         </div>
       </Layout>
