d0baea43dd0c9d108a0f4f521f5215e7864c7d5a
Victoria Lim <lim.t.victoria@gmail.com>
2022-12-29 14:01:12 -0800

    small updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,7 @@ class About extends React.Component {
         <div id="main">
           <section id="content" className="main">
             <span className="image main"><img src={about_img} alt="" /></span>
-            <p>Victoria Lim is a Senior Technical Writer at Imply Data. She completed her PhD in Chemistry at the University of California, Irvine, where she was awarded a Graduate Research Fellowship by the National Science Foundation (NSF GRFP). She also conducted research abroad in Sweden, funded by the NSF Graduate Research Opportunities Worldwide (NSF GROW) award.</p>
+            <p>Victoria Lim is a Senior Technical Writer at Imply Data. She completed her PhD in Chemistry at the University of California, Irvine, where she was awarded a Graduate Research Fellowship by the National Science Foundation (NSF GRFP). During her PhD, she volunteered annually as a peer reviewer for the UCI Department of Chemistry for other applicants to the NSF GRFP. Victoria also successfully applied to the NSF Graduate Research Opportunities Worldwide (NSF GROW) award to conduct research abroad in Sweden.</p>
             <p>She provides editing services for students and researchers in the sciences. She has worked with clients applying for the following:
             <ul>
               <li>Full-time industry positions</li>
@@ -26,8 +26,9 @@ class About extends React.Component {
               <li>Medical school and residency admissions</li>
             </ul>
             </p>
-            <p>She was an application reviewer for the National Center for Women & Information Technology Award for Aspirations in Computing (high school level) and NCWIT Collegiate Award (undergraduate and graduate level).</p>
+            <p>Victoria was an application reviewer for the National Center for Women & Information Technology Award for Aspirations in Computing (high school level) and NCWIT Collegiate Award (undergraduate and graduate level).</p>
             <p>Her areas of expertise include chemistry, biology, physics, and software development. For these areas, she offers more extensive editing services for scientific manuscripts, theses, and grant applications.</p>
+            <p>Read some of the feedback from her editing in the <a href="/testimonials">Testimonials</a>.</p>
           </section>
         </div>
       </Layout>
